7b9d655c4d15f8e9fb113df9bbcf34fdb2d0fce8
Imac <imac@Imacs-iMac.local>
2022-12-19 13:42:39 +0700

    update secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -25,5 +25,23 @@ export const authOptions = {
       },
     }),
   ],
+  // add secret
+  secret: process.env.NEXT_PUBLIC_SECRET,
+  callbacks: {
+    async jwt({ token, user, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+        token.user = user;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      session.user = token.user;
+      return session;
+    },
+  },
 };
 export default NextAuth(authOptions);
